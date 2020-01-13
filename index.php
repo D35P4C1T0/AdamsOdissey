9deac0a4aea0159c77ab9821bb1ed669bd9c267e
Matteo-x220 <matteosasoris@gmail.com>
2020-01-13 10:30:49 +0100

    Final touches for the presentation

--- index.php
+++ index.php
@@ -6,19 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.rawgit.com/Chalarangelo/mini.css/v3.0.1/dist/mini-default.min.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="shortcut icon" href="logoMini.jpg" />
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="shortcut icon" href="/media/logoMini.jpg" />
     <title>Adam's Odyssey</title>
 </head>
 
 <body>
     <header>
-        <a href="index.html" class="logo"><img id="logoImg" src="logo.jpg"></a>
-        <a href="cities/newyork.html" class="button cities" style="background-color: #EACD65; position: absolute; top: 1.3em;left: 12.5em; background-color: #E54142;color: white; font-size: 1em;font-family:Georgia; height: 3em; text-transform:none; padding-top: 0.2em;">New York</a>
-        <a href="cities/washington.html" class="button" style="background-color: #EACD65; position: absolute; top: 1.3em;left: 20em; background-color: #E54142;color: white; font-size: 1em;font-family:Georgia; height: 3em; text-transform:none; padding-top: 0.2em;">Washington D.C.</a>
-        <a href="cities/losangeles.html" class="button" style="background-color: #EACD65; position: absolute; top: 1.3em;left: 31em; background-color: #E54142;color: white; font-size: 1em;font-family:Georgia; height: 3em; text-transform:none; padding-top: 0.2em;">San Diego</a>
-        <a href="cities/losangeles.html" class="button" style="background-color: #EACD65; position: absolute; top: 1.3em;left: 39em; background-color: #E54142;color: white; font-size: 1em;font-family:Georgia; height: 3em; text-transform:none; padding-top: 0.2em;">Los Angeles</a>
-        <a href="cities/portland.html" class="button" style="background-color: #EACD65; position: absolute; top: 1.3em;left: 48em; background-color: #E54142;color: white; font-size: 1em;font-family:Georgia; height: 3em; text-transform:none; padding-top: 0.2em;">Portland</a>
+        <a href="index.html" class="logo"><img id="logoImg" src="/media/logo.jpg"></a>
+        <a href="/cities/newyork.html" class="button cities" style="background-color: #EACD65; position: absolute; top: 1.3em;left: 12.5em; background-color: #E54142;color: white; font-size: 1em;font-family:Georgia; height: 3em; text-transform:none; padding-top: 0.2em;">New York</a>
+        <a href="/cities/washington.html" class="button" style="background-color: #EACD65; position: absolute; top: 1.3em;left: 20em; background-color: #E54142;color: white; font-size: 1em;font-family:Georgia; height: 3em; text-transform:none; padding-top: 0.2em;">Washington D.C.</a>
+        <a href="/cities/losangeles.html" class="button" style="background-color: #EACD65; position: absolute; top: 1.3em;left: 31em; background-color: #E54142;color: white; font-size: 1em;font-family:Georgia; height: 3em; text-transform:none; padding-top: 0.2em;">San Diego</a>
+        <a href="/cities/losangeles.html" class="button" style="background-color: #EACD65; position: absolute; top: 1.3em;left: 39em; background-color: #E54142;color: white; font-size: 1em;font-family:Georgia; height: 3em; text-transform:none; padding-top: 0.2em;">Los Angeles</a>
+        <a href="/cities/portland.html" class="button" style="background-color: #EACD65; position: absolute; top: 1.3em;left: 48em; background-color: #E54142;color: white; font-size: 1em;font-family:Georgia; height: 3em; text-transform:none; padding-top: 0.2em;">Portland</a>
     </header>
     <div class="container">
         <div class="row">
@@ -51,7 +51,7 @@
             </div>
             <div class="col-sm-8">
                 <h1>Your itinerary</h1>
-                <?php include 'script.php' ?>
+                <?php include 'script.php'?>
             </div>
         </div>
     </div>
